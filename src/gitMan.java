1807b8c91f539513a993d7bee69833de34165194
Rana g <babaralie@gmail.com>
2024-10-10 19:58:58 -0700

    palindrome code is this is

---
+++ src/gitMan.java
@@ -0,0 +1,13 @@
+public class gitMan {
+    public static void main(String[] args) {
+        String word="levls";
+        StringBuilder obj=new StringBuilder(word);
+        String reverse=obj.reverse().toString();
+        if (word.equals(reverse)){
+            System.out.println("word is palindrome");
+        }else {
+            System.out.println("not palindrome");
+        }
+
+    }
+}
